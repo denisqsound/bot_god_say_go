5e51e5ee58996ce869a463d49ec942d506641f12
Denis Khakhalkin <denisqsound@gmail.com>
2021-07-05 08:27:20 +0300

    asdasd

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"fmt"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"time"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func init() {
@@ -13,102 +16,49 @@ func init() {
 	}
 }
 
-// Точка входа программы
 func main() {
+	var ChatId int64
 	botToken, _ := os.LookupEnv("TOKEN")
-
 	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
 		log.Panic(err)
 	}
+
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
+	u.Timeout = 10
 	updates, err := bot.GetUpdatesChan(u)
 
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
+	// TODO Получить ID чата
+	ChatId = 123
+	//fmt.Println(updates)
+	// TODO Начать в цикле слать картинки
+	for {
+		//tgbotapi.NewPhotoUpload(ChatId,"https://assets.pinterest.com/ext/embed.html?id=68117013100642866" )
+		msg := tgbotapi.NewMessage(ChatId, "qwe")
 		bot.Send(msg)
+		time.Sleep(10 * time.Second)
 	}
 
-	//////////////////////////////////////////////////
-
-	//botToken, _ := os.LookupEnv("TOKEN")
-	//botApi := "https://api.telegram.org/bot"
-	//botUrl := botApi + botToken
-	//offset := 0
-
-	//updates, err := getUpdates(botUrl, offset)
-	//if err != nil {
-	//	log.Fatalln("Unable to make request: ", err)
-	//}
-
-	//c := cron.New()
-	//c.AddFunc("@every 10s", func() { SendMessage(botUrl, updates) })
-	//c.Start()
-
-	//for {
-	//	updates, err := getUpdates(botUrl, offset)
-	//	if err != nil {
-	//		log.Fatalln("Unable to make request: ", err)
-	//	}
-	//
-	//	for _, update := range updates {
-	//		time.Sleep(time.Second * 5)
-	//		Respond(botUrl, update)
-	//		offset = update.UpdateId + 1
+	//for update := range updates {
+	//	log.Printf("BOT UPDATE : %s", update.Message)
+	//	if update.Message == nil { // ignore any non-Message Updates
+	//		continue
+	//		//tgbotapi.NewPhotoUpload(update.Message.Chat.ID,"https://assets.pinterest.com/ext/embed.html?id=68117013100642866")
+	//		//tgbotapi.NewMessage(update.Message.Chat.ID, "qwe")
 	//
 	//	}
-	//	fmt.Println(updates)
+	//	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+	//	ChatId = update.Message.Chat.ID
+	//
+	//	//log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+	//
+	//	//msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+	//	//msg.ReplyToMessageID = update.Message.MessageID
+	//	//
+	//	//bot.Send(msg)
 	//}
 
 }
-
-// Запрос обновлений
-//func getUpdates(botUrl string, offset int) ([]Update, error) {
-//	ro := &grequests.RequestOptions{Params: map[string]string{"offset": strconv.Itoa(offset)}}
-//	res, err := grequests.Get(botUrl+"/getUpdates", ro)
-//	if err != nil {
-//		log.Fatalln("Unable to make request: ", err)
-//	}
-//
-//	resp := ResResponse{}
-//	err = res.JSON(&resp)
-//	if err != nil {
-//		log.Fatalln("Unable to make request: ", err)
-//	}
-//
-//	return resp.Result, nil
-//
-//}
-
-// Отвечает на обновление
-//func SendMessage(botUrl string, update Update) {
-//	botMessage := BotMessage{}
-//	botMessage.ChatId = update.Message.Chat.ChatId
-//	botMessage.Photo = "https://www.pinterest.com/pin/68117013100642866/"
-//
-//	ro := &grequests.RequestOptions{
-//		Headers: map[string]string{"Content-Type": "application/json"},
-//		JSON:    &botMessage,
-//	}
-//	_, err := grequests.Post(botUrl+"/sendPhoto", ro)
-//	if err != nil {
-//		log.Fatalln("Unable to make request: ", err)
-//	}
-//
-//	return
-//
-//}
