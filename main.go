801ea0e1de4c1496a693cba74622899faeaacb40
denisqsound <denisqsound@gmail.com>
2021-07-10 17:31:47 +0300

    Change read image from hard disk

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/joho/godotenv"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"os"
@@ -15,28 +17,7 @@ func init() {
 	}
 }
 
-var IMG = map[int]string{
-	1:  "https://disk.yandex.ru/i/MYT2df5Iyyzg1g",
-	2:  "https://disk.yandex.ru/i/RAHfOqdMJ-5bJQ",
-	3:  "https://disk.yandex.ru/i/Agd2Fw4yTa4m6A",
-	4:  "https://disk.yandex.ru/i/m6iPlUjpPfaMFg",
-	5:  "https://disk.yandex.ru/i/QU-j_ziPBl41Yw",
-	6:  "https://disk.yandex.ru/i/U1z4iO84HrT_ZA",
-	7:  "https://disk.yandex.ru/i/7NUlGmFqkWU9Yg",
-	8:  "https://disk.yandex.ru/i/m8Dr6_duw_Ecug",
-	9:  "https://disk.yandex.ru/i/ABWTvj9aQR6p6A",
-	10: "https://disk.yandex.ru/i/68FxAdPv9Yatmw",
-	11: "https://disk.yandex.ru/i/VLv5MzsbZC9raA",
-	12: "https://disk.yandex.ru/i/oUENMBUeQhhBiA",
-	13: "https://disk.yandex.ru/i/C7z9tjPdymgzzA",
-	14: "https://disk.yandex.ru/i/2CA3NuNzJ2fg5w",
-	15: "https://disk.yandex.ru/i/KaqkPzLXd4J1nA",
-	16: "https://disk.yandex.ru/i/FDagx_bvm2ciRQ",
-	17: "https://disk.yandex.ru/i/jgG5lFMZCrmsXg",
-	18: "https://disk.yandex.ru/i/6fR444jtFfkOYg",
-	19: "https://disk.yandex.ru/i/DeybnWhYkNk05A",
-	20: "https://disk.yandex.ru/i/N6jKIbd969MdcA",
-}
+// 401 картинка скачана 10 июля 2021 года
 
 func main() {
 	botToken, _ := os.LookupEnv("TOKEN")
@@ -53,6 +34,7 @@ func main() {
 	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
+
 		log.Printf("BOT UPDATE : %s", update.Message)
 		if update.Message == nil { // ignore any non-Message Updates
 			continue
@@ -62,10 +44,29 @@ func main() {
 
 		if update.Message.Text == "/start" {
 			for {
-				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-				msg := tgbotapi.NewPhotoShare(update.Message.Chat.ID, RandomImage())
-				time.Sleep(24 * time.Hour)
-				bot.Send(msg)
+				fileName := fmt.Sprintf("bible_app/bible-%d.JPG", Random(1, 402))
+
+				photoBytes, err := ioutil.ReadFile(fileName)
+				if err != nil {
+					log.Fatalln("Unable read file: ", err)
+				}
+
+				photoFileBytes := tgbotapi.FileBytes{
+					Name:  "picture",
+					Bytes: photoBytes,
+				}
+
+				time.Sleep(5 * time.Second)
+				_, err = bot.Send(tgbotapi.NewPhotoUpload(update.Message.Chat.ID, photoFileBytes))
+				if err != nil {
+					log.Fatalln("Unable send photo: ", err)
+				}
+
+				_, err = bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, fileName))
+				if err != nil {
+					log.Fatalln("Unable send photo: ", err)
+				}
+
 			}
 
 		}
@@ -83,7 +84,7 @@ func Random(min, max int) int {
 	}
 }
 
-func RandomImage() string {
-	return IMG[Random(1, 20)]
+//func RandomImage() string {
+//	return IMG[Random(1, 20)]
 
-}
+//}
